acc6432490328771ccea8e3533246e804591043c
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-21 20:54:46 +0200

    Initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#0067FF",
+        yellowColor: "#F5B43B",
+        purpleColor: "#9771FF",
+        irisBlueColor: "#181A1E",
+        headingColor: "#4E545F",
+      },
+      boxShadow: {
+        panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px",
+      },
+    },
+  },
+  plugins: [],
+};
