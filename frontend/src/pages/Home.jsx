6336862117ebffd12c78a23b5fddf965e7950ad2
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-24 00:29:56 +0200

    Home page How it's works in sections

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom";
 import heroImg01 from "../assets/images/hero-img01.png";
 import heroImg02 from "../assets/images/hero-img02.png";
 import heroImg03 from "../assets/images/hero-img03.png";
+import icon01 from "../assets/images/icon01.png";
+import icon02 from "../assets/images/icon02.png";
+import icon03 from "../assets/images/icon03.png";
+import { BsArrowRight } from "react-icons/bs";
 export default function Home() {
   return (
     <>
@@ -47,7 +52,6 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-              {/* Hero contents */}
 
               <div className="flex gap-[30px] justify-end">
                 <div>
@@ -60,6 +64,86 @@ export default function Home() {
                     className=" w-full mb-[30px]"
                   />
                   <img src={heroImg03} alt="Image" className=" w-full" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* Hero section end */}
+
+        <section>
+          <div className="container">
+            <div className=" lg:w-[470px] mx-auto">
+              <h2 className=" heading text-center">
+                Providing the best medical services
+              </h2>
+              <p className=" text_para text-center">
+                World-class care for everyone. Our Health System offers
+                unmatched, expert health care
+              </p>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+              <div className=" py-[30px] px-5">
+                <div className=" flex items-center justify-center">
+                  <img src={icon01} alt="Image" />
+                </div>
+                <div className=" mt-[30px]">
+                  <h2 className=" text-[26px] leading-9 text-headingColor font-[700] text-center">
+                    Find a Doctors
+                  </h2>
+                  <p className=" text-[16px] leading-7 text-headingColor font-[400] mt-4 text-center">
+                    World-class care for everyone. Our health system offers
+                    unmatched, expert health care. Form the lab to the clinic.
+                  </p>
+                  <Link
+                    to={"/doctors"}
+                    className=" w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                  >
+                    <BsArrowRight className=" group-hover:text-white w-6 h-5" />
+                  </Link>
+                </div>
+              </div>
+
+              <div className=" py-[30px] px-5">
+                <div className=" flex items-center justify-center">
+                  <img src={icon02} alt="Image" />
+                </div>
+                <div className=" mt-[30px]">
+                  <h2 className=" text-[26px] leading-9 text-headingColor font-[700] text-center">
+                    Find a Location
+                  </h2>
+                  <p className=" text-[16px] leading-7 text-headingColor font-[400] mt-4 text-center">
+                    World-class care for everyone. Our health system offers
+                    unmatched, expert health care. Form the lab to the clinic.
+                  </p>
+                  <Link
+                    to={"/doctors"}
+                    className=" w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                  >
+                    <BsArrowRight className=" group-hover:text-white w-6 h-5" />
+                  </Link>
+                </div>
+              </div>
+
+              <div className=" py-[30px] px-5">
+                <div className=" flex items-center justify-center">
+                  <img src={icon03} alt="Image" />
+                </div>
+                <div className=" mt-[30px]">
+                  <h2 className=" text-[26px] leading-9 text-headingColor font-[700] text-center">
+                    Book Appointment
+                  </h2>
+                  <p className=" text-[16px] leading-7 text-headingColor font-[400] mt-4 text-center">
+                    World-class care for everyone. Our health system offers
+                    unmatched, expert health care. Form the lab to the clinic.
+                  </p>
+                  <Link
+                    to={"/doctors"}
+                    className=" w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                  >
+                    <BsArrowRight className=" group-hover:text-white w-6 h-5" />
+                  </Link>
                 </div>
               </div>
             </div>
