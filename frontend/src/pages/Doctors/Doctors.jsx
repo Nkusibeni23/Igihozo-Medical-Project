78e36b76121cd2894d635c8cd90e5fe8ba81c2b8
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-25 15:37:21 +0200

    complete find a doctors sections

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,3 +1,47 @@
+import { doctors } from "../../assets/data/doctors";
+import DoctorCard from "../../components/Doctors/DoctorCard";
+import Testimonial from "../../components/Testimonial/Testimonial";
+
 export default function Doctors() {
-  return <div>Doctors</div>;
+  return (
+    <>
+      <section className=" bg-[#fff9ea]">
+        <div className=" container text-center">
+          <h2 className="heading">Find a Doctor</h2>
+          <div className=" max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-xl flex items-center justify-between">
+            <input
+              type="search"
+              className="py-4 pl-4 pr-2 px-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-gray-400"
+              placeholder="Search Doctor..."
+            />
+            <button className=" btn mt-0 rounded-lg">Search</button>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-5">
+            {doctors.map((doctors) => (
+              <DoctorCard key={doctors._id} doctors={doctors} />
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className=" xl:w-4/5 mx-auto py-2 lg:py-4">
+            <h2 className=" heading text-center">What our patient say</h2>
+            <p className=" text_para text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+              sunt voluptatem, nostrum consequatur sed ut cum, iusto culpa, non
+              eius aliquam consectetur vel earum velit tenetur architecto
+              dignissimos soluta odit?
+            </p>
+          </div>
+          <Testimonial />
+        </div>
+      </section>
+    </>
+  );
 }
--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,3 +1,16 @@
+import { services } from "../assets/data/services";
+import ServicesCard from "../components/Services/ServicesCard";
+
 export default function Services() {
-  return <div>Services</div>;
+  return (
+    <section>
+      <div className="container">
+        <div className=" grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px]">
+          {services.map((item, index) => (
+            <ServicesCard key={index} index={index} item={item} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
