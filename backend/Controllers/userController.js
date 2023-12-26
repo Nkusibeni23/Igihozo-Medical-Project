5939092b21ecb1e114bb48aa323f0b82c3f57aeb
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-26 23:56:10 +0200

    complete DeleteUser(id) api section

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -26,3 +26,23 @@ export const updateUser = async (req, res) => {
     });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    // Get the user by their ID from the database
+    await UserSchema.findByIdAndDelete(id);
+    if (!updateUser) return res.status(404).send("No user with that ID");
+    res.status(200).json({
+      success: true,
+      message: "SuccessFully Deleted",
+    });
+  } catch (error) {
+    console.log(`Error in deleting a user : ${error}`);
+    res.status(500).json({
+      success: false,
+      message: "Failed to delete",
+      error: `Server Error : ${error}`,
+    });
+  }
+};
