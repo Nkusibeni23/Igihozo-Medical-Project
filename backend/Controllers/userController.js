fd67fcab93d9f32db377593144d6d0b601c38bee
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-26 23:53:02 +0200

    Complete updateUser api section

---
+++ backend/Controllers/userController.js
@@ -0,0 +1,28 @@
+import UserSchema from "../models/UserSchema";
+
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    // Get the user by their ID from the database
+    const updateUser = await UserSchema.findByIdAndUpdate(
+      id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    if (!updateUser) return res.status(404).send("No user with that ID");
+    res.status(200).json({
+      success: true,
+      message: "SuccessFully Update",
+      data: updateUser,
+    });
+  } catch (error) {
+    console.log(`Error in updating a user : ${error}`);
+    res.status(500).json({
+      success: false,
+      message: "Failed to update",
+      error: `Server Error : ${error}`,
+    });
+  }
+};
