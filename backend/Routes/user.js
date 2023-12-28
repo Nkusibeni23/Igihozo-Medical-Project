f42f2424aaaf31e12e84767ee0843a1336e33ab8
Benny Chrispin <ndizibaidu23@gmail.com>
2023-12-28 14:09:23 +0200

    routes of getUserProfile and getMyAppointments

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
   deleteUser,
   getAllUser,
+  getMyAppointments,
   getSingleUser,
+  getUserProfile,
   updateUser,
 } from "../Controllers/userController.js";
 import { authenticate, restrict } from "../auth/verifyToken.js";
@@ -13,5 +15,12 @@ router.get("/:id", authenticate, restrict(["patient"]), getSingleUser);
 router.get("/", authenticate, restrict(["admin"]), getAllUser);
 router.put("/:id", authenticate, restrict(["patient"]), updateUser);
 router.delete("/:id", authenticate, restrict(["patient"]), deleteUser);
+router.get("/profile/me", authenticate, restrict(["patient"]), getUserProfile);
+router.get(
+  "appointments",
+  authenticate,
+  restrict(["patient"]),
+  getMyAppointments
+);
 
 export default router;
